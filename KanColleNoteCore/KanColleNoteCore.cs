3873865e46615874252963ed005845fd9d8d181e
爱琴炫彩 <aiqinxuancai@163.com>
2017-08-01 16:13:37 +0800

    添加项目文件。

---
+++ KanColleNoteCore/KanColleNoteCore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleNoteCore
+{
+    public class KanColleNoteCore
+    {
+        /// <summary>
+        /// 主程序调用，将数据传输到这里
+        /// </summary>
+        /// <param name="path">url路径</param>
+        /// <param name="json">json数据</param>
+        static bool RecvPack(string path, string json)
+        {
+            //Pack分发
+
+
+            return false;
+
+        }
+
+
+
+
+    }
+}
