19121c51584870bfb67f54abf208cf3b2b6e71b9
爱琴炫彩 <aiqinxuancai@163.com>
2017-08-08 15:21:12 +0800

    完成快速设置代理的逻辑

--- KanColleNote/UI/SettingPage.xaml.cs
+++ KanColleNote/UI/SettingPage.xaml.cs
@@ -40,17 +40,45 @@ namespace KanColleNote.UI
 
         private void buttonSetting_Click(object sender, RoutedEventArgs e)
         {
+            SaveConfig();
+        }
+
+        private void SaveConfig()
+        {
             JObject j = JObject.Parse(JsonConvert.SerializeObject(root));
             SpeedConfig.jsonTable.Merge(j);
             SpeedConfig.Save();
         }
 
+
         private async void buttonAutoSetProxy_Click(object sender, RoutedEventArgs e)
         {
+            buttonAutoSetProxy.IsEnabled = false;
             AutoProxyService auto = new AutoProxyService();
-            await auto.AutoSetting();
+            string data = await auto.AutoSetting();
+
+            if (string.IsNullOrEmpty(data))
+            {
+                //失败
+                MessageBox.Show("没有发现ACGPower、岛风Go、Shadowsocks进程！", "提示", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                JObject json = JObject.Parse(data);
+
+
+                root.ProxyIP = "127.0.0.1";
+                root.ProxyPort = json["port"].Value<int>();
+                //成功
+                MessageBox.Show($"发现{json["type"]}，代理端口设置为：{json["port"]}", "提示", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                Debug.WriteLine(data);
+
+
+                SaveConfig();
+            }
+
             Debug.WriteLine("运行完毕");
-            
+            buttonAutoSetProxy.IsEnabled = true ;
 
         }
     }
